4250c9670cc8bf192cb04866f4c570bc96beb9c9
patrick lo <patrick.jh.lo@hotmail.com>
2015-04-22 15:08:03 +0800

    initial setup

---
+++ src/main/groovy/TestGroovy.groovy
@@ -0,0 +1,5 @@
+/**
+ * Created by patrick on 2015/4/22.
+ */
+class TestGroovy {
+}
---
+++ src/main/java/TestDriveGroovy.java
@@ -0,0 +1,9 @@
+/**
+ * Created by patrick on 2015/4/22.
+ */
+public class TestDriveGroovy {
+
+    public static void main(String [] args){
+
+    }
+}
